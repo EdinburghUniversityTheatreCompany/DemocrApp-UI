71fc5d3314d4e44b410c0b87303f9ececc5d2e78
rphi <8237136+rphi@users.noreply.github.com>
2018-02-14 15:04:08 +0000

    Complete basic landing page and template. Bootstrap working.

---
+++ src/Landing.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import './assets/css/Landing.css';
+
+class Landing extends Component {
+  render() {
+    return (
+        <div className="container">
+          <h3>Welcome to</h3>
+          <h1>DemocrApp</h1>
+          <p>
+            Please collect a voting token from the event organiser to start
+            your session.
+          </p>
+          <Button color="primary" size='lg' block>I've got my token!</Button>
+        </div>
+    );
+  }
+}
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './vendor/bootstrap/css/bootstrap.min.css';
+import './vendor/bootstrap/js/bootstrap.min.js';
+import Landing from './Landing';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Landing />, document.getElementById('root'));
 registerServiceWorker();
